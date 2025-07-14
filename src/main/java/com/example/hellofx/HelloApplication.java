a32499cf3ccf4b069f639f9f80d5852c02bea330
Shaheer Shaikh <shaheercodes.allday@gmail.com>
2025-07-14 16:21:11 +0530

    Worked with scene builder for switching the scenes

--- src/main/java/com/example/hellofx/HelloApplication.java
+++ src/main/java/com/example/hellofx/HelloApplication.java
@@ -6,28 +6,23 @@ import javafx.scene.Parent;
 import javafx.scene.Scene;
 import javafx.stage.Stage;
 
+import java.io.IOException;
+import java.net.URL;
 import java.util.Objects;
 
 public class HelloApplication extends Application {
-
     @Override
-    public void start(Stage stage) throws Exception {
-        Parent root = FXMLLoader.load(
-                Objects.requireNonNull(
-                        getClass().getResource("/com.example.hellofx/views/action.fxml")
-                )
-        );
-
-        Scene scene = new Scene(root);
-
-        String css = Objects.requireNonNull(
-                getClass().getResource("/com.example.hellofx/assets/style.css")
-            ).toExternalForm();
-        scene.getStylesheets().add(css);
-
-        stage.setTitle("Java CSS and Transitions");
-        stage.setScene(scene);
-        stage.show();
+    public void start(Stage stage) {
+        Parent root;
+        try {
+            URL initialFXML = getClass().getResource("/com.example.hellofx/views/Scene01.fxml");
+            root = FXMLLoader.load(Objects.requireNonNull(initialFXML));
+            Scene scene = new Scene(root);
+            stage.setScene(scene);
+            stage.show();
+        } catch(NullPointerException | IOException exception) {
+            System.out.println("FXML file not found - " + exception.getMessage());
+        }
     }
 
     public static void main(String[] args) {
--- src/main/java/com/example/hellofx/controllers/Controller.java
+++ src/main/java/com/example/hellofx/controllers/Controller.java
@@ -1,24 +1,44 @@
 package com.example.hellofx.controllers;
 
-import javafx.animation.TranslateTransition;
 import javafx.event.ActionEvent;
-import javafx.fxml.FXML;
-import javafx.scene.shape.Circle;
-import javafx.util.Duration;
+import javafx.fxml.FXMLLoader;
+import javafx.scene.Node;
+import javafx.scene.Parent;
+import javafx.scene.Scene;
+import javafx.stage.Stage;
+
+import java.io.IOException;
+import java.net.URL;
+import java.util.Objects;
 
 public class Controller {
-    @FXML
-    private Circle myCircle;
-    private int a = 0, b = 300;
+    private Parent root;
+    private Scene scene;
+    private Stage stage;
 
+    public void switchToScene01(ActionEvent event) {
+        try {
+            URL rootUrl = getClass().getResource("/com.example.hellofx/views/Scene01.fxml");
+            root = FXMLLoader.load(Objects.requireNonNull(rootUrl));
+            scene = new Scene(root);
+            stage = (Stage) ((Node) event.getSource()).getScene().getWindow();
+            stage.setScene(scene);
+            stage.show();
+        } catch(IOException | NullPointerException exception) {
+            System.out.println("Error while switching window!");
+        }
+    }
 
-    public void moveX(ActionEvent e) {
-        TranslateTransition move = new TranslateTransition(Duration.seconds(1), myCircle);
-        move.setFromX(a);
-        move.setToX(b);
-        move.play();
-        int temp = a;
-        a = b;
-        b = temp;
+    public void switchToScene02(ActionEvent event) {
+        try {
+            URL rootUrl = getClass().getResource("/com.example.hellofx/views/Scene02.fxml");
+            root = FXMLLoader.load(Objects.requireNonNull(rootUrl));
+            scene = new Scene(root);
+            stage = (Stage) ((Node) event.getSource()).getScene().getWindow();
+            stage.setScene(scene);
+            stage.show();
+        } catch(IOException | NullPointerException exception) {
+            System.out.println("Error while switching window!");
+        }
     }
 }
